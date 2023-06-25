c75b02854e8cd6881f0f390a7a443a93d5deee85
ScarlettChen0111 <scarlettchan0111@gmail.com>
2023-06-25 10:41:54 -0400

    ..

--- CLG-CLInternational.js
+++ CLG-CLInternational.js
@@ -1,28 +1,28 @@
-let CL = document.getElementById("CLText");
-let CLsubmit = document.getElementById("CLsubmit");
-let CLguide = document.getElementById("CLguide");
-CLsubmit.addEventListener("click", coverLetter);
+let CLI = document.getElementById("CLIText");
+let CLIsubmit = document.getElementById("CLIsubmit");
+let CLIguide = document.getElementById("CLIguide");
+CLIsubmit.addEventListener("click", coverLetter);
 
 
 function coverLetter(){
   //read input//
-  let NAME = document.getElementById("CLname").value
-  let recipient = document.getElementById("CLrecipient").value
-  let job = document.getElementById("CLjobtitle").value
-  let company = document.getElementById("CLcompany").value
-  let years = document.getElementById("CLexperience").value
-  let previous = document.getElementById("CLprevious").value
-  let reason = document.getElementById("CLwhyimpress").value
-  let email = document.getElementById("CLemail").value
-  let phone = document.getElementById("CLnumber").value
-  let skills = document.getElementById("CLskills").value
-  let elaborate = document.getElementById("CLelaborate").value
-  let achievement = document.getElementById("CLachievements").value
-  let relevant = document.getElementById("CLrelevant").value 
-  let where = document.getElementById("CLwhere").value 
-  let previouscompany = document.getElementById("CLpreviouscompany").value
+  let NAME = document.getElementById("CLIname").value
+  let recipient = document.getElementById("CLIrecipient").value
+  let job = document.getElementById("CLIjobtitle").value
+  let company = document.getElementById("CLIcompany").value
+  let years = document.getElementById("CLIexperience").value
+  let previous = document.getElementById("CLIprevious").value
+  let reason = document.getElementById("CLIwhyimpress").value
+  let email = document.getElementById("CLIemail").value
+  let phone = document.getElementById("CLInumber").value
+  let skills = document.getElementById("CLIskills").value
+  let elaborate = document.getElementById("CLIelaborate").value
+  let achievement = document.getElementById("CLIachievements").value
+  let relevant = document.getElementById("CLIrelevant").value 
+  let where = document.getElementById("CLIwhere").value 
+  let previouscompany = document.getElementById("CLIpreviouscompany").value
 
-  let CLG = [`Dear ${recipient},<br><br>
+  let CLIG = [`Dear ${recipient},<br><br>
   My name is ${NAME}. I am writing to express my keen interest in the ${job} position at ${company}, as advertised on ${where}. With my strong qualifications and passion for ${relevant}, I am confident that I can make a valuable contribution to your organization.
   <br>  I have worked in <span style = "color: aqua">[mention specific areas or roles relevant to the position]</span> for ${years} , which have equipped me with a diverse skill set and an in-depth understanding of <span style = "color: aqua">[industry-specific knowledge]</span>. Throughout my career, I have consistently demonstrated exceptional ${skills}, such as ${elaborate}.
   <br>  At my previous position as ${previous} at <span style = "color: aqua">${previouscompany}</span>, I successfully ${achievement}. These experiences have honed my ability to <span style = "color: aqua">[describe key responsibilities or tasks relevant to the position]</span> and have given me a solid foundation in <span style = "color: aqua">[industry-specific knowledge]</span>.
@@ -34,14 +34,14 @@ function coverLetter(){
   <br><br>Sincerely,
   <br>${NAME}`]
   //clear page//
-  CL.innerHTML = "";
+  CLI.innerHTML = "";
   
 
   //Cover Letter//
   let min = 0;
-  let max = CLG.length;
+  let max = CLIG.length;
   let num = Math.floor(Math.random() * (max - min )) + min;
-  CL.innerHTML += CLG[`${num}`];
+  CLI.innerHTML += CLIG[`${num}`];
 
 }
 
@@ -53,11 +53,11 @@ toggleSwitch.addEventListener('change', function() {
   if (this.checked) {
     // Do something when switch is on
     console.log('Switch is on');
-    CLguide.innerHTML+="1. Fill out all input boxes using the format written in placeholders.<br><br>2. <span style ='color:aqua'> Highlighted</span> parts are specific contents that can vary greatly for different users. Fill out the highlighted parts with your own words and voices so that the tone sounds more customized.<br><br>3. The generated texts are editable on web page.<br><br>4. Scroll up to see the generated letter.<br><br>5. Make sure to double-check before you copy-paste and send it.";
+    CLIguide.innerHTML+="1. Fill out all input boxes using the format written in placeholders.<br><br>2. <span style ='color:aqua'> Highlighted</span> parts are specific contents that can vary greatly for different users. Fill out the highlighted parts with your own words and voices so that the tone sounds more customized.<br><br>3. The generated texts are editable on web page.<br><br>4. Scroll up to see the generated letter.<br><br>5. Make sure to double-check before you copy-paste and send it.";
 
   } else {
     // Do something when switch is off
     console.log('Switch is off');
-    CLguide.innerHTML=""; 
+    CLIguide.innerHTML=""; 
    }
 });
